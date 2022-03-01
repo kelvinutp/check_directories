8bb1758dab81ffbe22c156f592fab25e60fdb13f
kelvinutp <48689965+kelvinutp@users.noreply.github.com>
2022-03-01 13:34:18 -0500

    Add files via upload

---
+++ recursive_directories.py
@@ -0,0 +1,31 @@
+import os
+import re
+
+z=[]
+
+def recursive(x):
+    if os.path.isdir(x):
+        a=os.listdir(x)
+        b=0
+        t='n'
+        while b<len(a):
+            c=re.search(r'(.[a-z]*)$',a[b])#extraer la extension del archivo
+            if os.path.isdir(os.path.join(x,a[b])): #consulta si es directorio
+                if os.path.join(x,a[b]) not in z:
+                    recursive(os.path.join(x,a[b]))
+            elif c[1] in ['.txt','.csv']: #consulta si la carpeta actual contiene .txt o .csv
+                t='s'
+            b+=1
+        if t=='s':
+            z.append(x)
+        return
+    else:
+        print('la direccion ingresada no es un directorio')
+        return
+        
+
+# dire=input('Direccion: ')
+dire=r'C:\Users\Kelvin Kung\OneDrive\Kelvin K\LiCo-3-20210609T104246Z-001'
+recursive(dire)
+for a in z:
+    print(a)
